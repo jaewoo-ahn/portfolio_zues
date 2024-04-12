8cb65e7004297c9b4d6586e25957e1c3f62addd0
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-04-12 20:08:26 +0900

    feat

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,30 @@
 "use client";
-
-import React from "react";
-import { motion } from "framer-motion";
+import { motion, useInView, useScroll } from "framer-motion";
+import { useRef } from "react";
 
 const AboutPage = () => {
+  const containerRef = useRef();
+
+  const { scrollYProgress } = useScroll({ container: containerRef });
+
+  const skillRef = useRef();
+  const isSkillRefInView = useInView(skillRef, { margin: "-100px" });
+
+  const experienceRef = useRef();
+  const isExperienceRefInView = useInView(experienceRef, { margin: "-100px" });
+  const Skills = [
+    "React",
+    "JavaScript",
+    "TypeScript",
+    "Git",
+    "GitHub",
+    "SCSS",
+    "TailwindCSS",
+    "Styled",
+    "Flutter",
+    "Dart",
+  ];
+
   return (
     <motion.div
       className="h-full"
@@ -11,141 +32,193 @@ const AboutPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      {/* CMT container */}
-      <div className="">
-        {/* CMT text container */}
-        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 zl:gap-64">
-          {/* CMT biography */}
-          <div className="">
-            <div className="flex flex-col gap-12 justify-center">
-              <h1 className="font-bold text-2xl">BIOGRAPHY</h1>
-              <p className="text-lg">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-                harum quibusdam cupiditate nobis accusamus sed aut aperiam,
-                reiciendis numquam! Voluptas voluptatibus obcaecati dolore
-                itaque suscipit! Vel doloremque numquam quam nihil.
-              </p>
-              <span className="italic">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </span>
-              <div className="self-end">
-                <svg
-                  width="151"
-                  height="60"
-                  viewBox="0 0 151 60"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M17 7C21.6198 7 66.4751 1.41799 66.5556 2.22222C66.8095 4.76149 60.0318 9.96818 58.8333 11.1667C45.3004 24.6996 30.6855 37.2356 16.0556 49.5556C15.2582 50.227 0.640619 62.8672 2.05556 56.5C2.94108 52.5151 7.31735 52.0728 10.6667 52C19.5407 51.8071 28.7439 48.2229 37.2778 46.0556C43.6365 44.4406 50.1259 43.5329 56.4445 41.7778C68.489 38.4321 81.8706 32.7814 90.2222 23.1111C97.7346 14.4126 77.0755 15.1747 73.2778 16.6667C68.6613 18.4803 65.6335 27.61 63.7778 31.6667C63.2216 32.8824 57.8861 41.1076 59.2222 42.7778C61.0648 45.081 66.0405 44.0547 68.4445 43.8889C77.6362 43.255 86.8322 40.5481 94.8889 36C100.204 32.9994 104 30.1529 104 23.5C104 19.7047 101 17.2186 101 22.5556C101 35.115 111.748 40.5126 124 39C133.381 37.8419 145.852 26.6627 149.222 17.7778C150.833 13.532 141.577 15 139.222 15C133.856 15 134.178 22.89 138.056 25.5556C140.731 27.3946 143.427 29.2737 143.778 32.7778C144.368 38.6762 123.687 45.1523 119.944 46.4444C104.316 51.8399 87.6167 56 71 56"
-                    stroke="black"
-                    stroke-width="3"
-                    stroke-linecap="round"
-                  />
-                </svg>
-              </div>
-            </div>
-          </div>
-          {/* CMT skills */}
+      <div className="h-full overflow-scroll lg:flex" ref={containerRef}>
+        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-1/2">
           <div className="flex flex-col gap-12 justify-center">
-            <h1 className="font-bold text-2xl">SKILLS</h1>
-            <div className="flex gap-4 flex-wrap">
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                JavaScript
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                TypeScript
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                React.js
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Next.js
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                SCSS
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Tailwind CSS
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                MongoDB
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                PostgreSQL
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Node.js
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Nest.js
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Express.js
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Spring Boot
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                GraphQL
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Apollo
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Redux
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Framer Motion
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Three.js
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                WebGL
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Webpack
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Vite
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Docker
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                AWS
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Firebase
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Git
-              </div>
-              <div className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-                Figma
-              </div>
+            <h1 className="font-bold text-2xl">BIOGRAPHY</h1>
+            <p className="text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              harum quibusdam cupiditate nobis accusamus sed aut aperiam,
+              reiciendis numquam! Voluptas voluptatibus obcaecati dolore itaque
+              suscipit! Vel doloremque numquam quam nihil.
+            </p>
+            <span className="italic">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </span>
+            <div className="self-end">
+              <svg
+                width="151"
+                height="60"
+                viewBox="0 0 151 60"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M17 7C21.6198 7 66.4751 1.41799 66.5556 2.22222C66.8095 4.76149 60.0318 9.96818 58.8333 11.1667C45.3004 24.6996 30.6855 37.2356 16.0556 49.5556C15.2582 50.227 0.640619 62.8672 2.05556 56.5C2.94108 52.5151 7.31735 52.0728 10.6667 52C19.5407 51.8071 28.7439 48.2229 37.2778 46.0556C43.6365 44.4406 50.1259 43.5329 56.4445 41.7778C68.489 38.4321 81.8706 32.7814 90.2222 23.1111C97.7346 14.4126 77.0755 15.1747 73.2778 16.6667C68.6613 18.4803 65.6335 27.61 63.7778 31.6667C63.2216 32.8824 57.8861 41.1076 59.2222 42.7778C61.0648 45.081 66.0405 44.0547 68.4445 43.8889C77.6362 43.255 86.8322 40.5481 94.8889 36C100.204 32.9994 104 30.1529 104 23.5C104 19.7047 101 17.2186 101 22.5556C101 35.115 111.748 40.5126 124 39C133.381 37.8419 145.852 26.6627 149.222 17.7778C150.833 13.532 141.577 15 139.222 15C133.856 15 134.178 22.89 138.056 25.5556C140.731 27.3946 143.427 29.2737 143.778 32.7778C144.368 38.6762 123.687 45.1523 119.944 46.4444C104.316 51.8399 87.6167 56 71 56"
+                  stroke="black"
+                  stroke-width="3"
+                  stroke-linecap="round"
+                />
+              </svg>
             </div>
-            <svg
-              width="151"
-              height="60"
-              viewBox="0 0 151 60"
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: "10px" }}
+              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+              viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
             >
               <path
-                d="M17 7C21.6198 7 66.4751 1.41799 66.5556 2.22222C66.8095 4.76149 60.0318 9.96818 58.8333 11.1667C45.3004 24.6996 30.6855 37.2356 16.0556 49.5556C15.2582 50.227 0.640619 62.8672 2.05556 56.5C2.94108 52.5151 7.31735 52.0728 10.6667 52C19.5407 51.8071 28.7439 48.2229 37.2778 46.0556C43.6365 44.4406 50.1259 43.5329 56.4445 41.7778C68.489 38.4321 81.8706 32.7814 90.2222 23.1111C97.7346 14.4126 77.0755 15.1747 73.2778 16.6667C68.6613 18.4803 65.6335 27.61 63.7778 31.6667C63.2216 32.8824 57.8861 41.1076 59.2222 42.7778C61.0648 45.081 66.0405 44.0547 68.4445 43.8889C77.6362 43.255 86.8322 40.5481 94.8889 36C100.204 32.9994 104 30.1529 104 23.5C104 19.7047 101 17.2186 101 22.5556C101 35.115 111.748 40.5126 124 39C133.381 37.8419 145.852 26.6627 149.222 17.7778C150.833 13.532 141.577 15 139.222 15C133.856 15 134.178 22.89 138.056 25.5556C140.731 27.3946 143.427 29.2737 143.778 32.7778C144.368 38.6762 123.687 45.1523 119.944 46.4444C104.316 51.8399 87.6167 56 71 56"
-                stroke="black"
-                stroke-width="3"
-                stroke-linecap="round"
-              />
-            </svg>
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+            </motion.svg>
           </div>
-          {/* CMT experience */}
-          <div className="">EXPERIENCE</div>
+          <div className="flex flex-col gap-12 justify-center" ref={skillRef}>
+            <motion.h1
+              initial={{ x: "-300px" }}
+              animate={isSkillRefInView ? { x: 0 } : {}}
+              transition={{ delay: 0.2 }}
+              className="font-bold text-2xl"
+            >
+              SKILLS
+            </motion.h1>
+            <motion.div
+              initial={{ x: "-300px" }}
+              animate={isSkillRefInView ? { x: 0 } : {}}
+              className="flex gap-4 flex-wrap"
+            >
+              {Skills.map((res, idx) => {
+                return (
+                  <div
+                    key={idx}
+                    className="rounded  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black"
+                  >
+                    {res}
+                  </div>
+                );
+              })}
+            </motion.div>
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: "10px" }}
+              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
+            >
+              <path
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+            </motion.svg>
+          </div>
+          <div
+            className="flex flex-col gap-12 justify-center pb-48"
+            ref={experienceRef}
+          >
+            <motion.h1
+              initial={{ x: "-300px" }}
+              animate={isExperienceRefInView ? { x: "0" } : {}}
+              transition={{ delay: 0.2 }}
+              className="font-bold text-2xl"
+            >
+              EXPERIENCE
+            </motion.h1>
+            <motion.div
+              initial={{ x: "-300px" }}
+              animate={isExperienceRefInView ? { x: "0" } : {}}
+              className=""
+            >
+              <div className="flex justify-between h-48">
+                <div className="w-1/3 ">
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Senior JavaScript Engineer
+                  </div>
+                  <div className="p-3 text-sm italic">
+                    I led web development, offering expertise in JavaScript
+                    frameworks.
+                  </div>
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2024 - Present
+                  </div>
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    Apple
+                  </div>
+                </div>
+                <div className="w-1/6 flex justify-center">
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                <div className="w-1/3 "></div>
+              </div>
+              <div className="flex justify-between h-48">
+                <div className="w-1/3 "></div>
+                <div className="w-1/6 flex justify-center">
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                <div className="w-1/3 ">
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Senior React Developer
+                  </div>
+                  <div className="p-3 text-sm italic">
+                    I spearheaded React-based application development,
+                    leveraging advanced skills.
+                  </div>
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2019 - 2024
+                  </div>
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    Microsoft
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-between h-48">
+                <div className="w-1/3 ">
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Freelancer
+                  </div>
+                  <div className="p-3 text-sm italic">
+                    I provided web solutions, applying a range of technologies
+                    to address client requirements.
+                  </div>
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2010 - 2019
+                  </div>
+                </div>
+                <div className="w-1/6 flex justify-center">
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                <div className="w-1/3 "></div>
+              </div>
+            </motion.div>
+          </div>
         </div>
-        {/*CMT  SVG */}
-        <div className="hidden "></div>
+        <div className="hidden lg:block w-1/3 sticky top-0 z-30 xl:w-1/2"></div>
       </div>
     </motion.div>
   );
