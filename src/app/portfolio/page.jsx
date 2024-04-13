8af71c57e707c337bebf85bf4c14b8d8e9839353
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-04-13 20:49:52 +0900

    ref

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,9 +1,12 @@
 "use client";
 
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const PortfolioPage = () => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({ target: ref });
+  const x = useTransform(scrollYProgress, [0, 1], ["0%,100%"]);
   return (
     <motion.div
       className="h-full"
@@ -11,7 +14,11 @@ const PortfolioPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      About
+      <div className="h-[600vh]">
+        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
+          My Works
+        </div>
+      </div>
     </motion.div>
   );
 };
