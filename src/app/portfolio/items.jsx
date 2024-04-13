9dac923d853e2b1b48ff315b5f02ae5034ddcecd
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-04-13 22:00:12 +0900

    items

---
+++ src/app/portfolio/items.jsx
@@ -0,0 +1,36 @@
+const items = [
+  {
+    id: 1,
+    color: "from-red-300 to-blue-300",
+    title: "React Commerce",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fsvgsilh.com%2Fsvg%2F2099128.svg&tbnid=eeNPqSlmL4oxpM&vet=12ahUKEwjl96SpnL-FAxWnfvUHHZPDDesQMygBegQIARBU..i&imgrefurl=https%3A%2F%2Fsvgsilh.com%2Fko%2Fimage%2F2099128.html&docid=1Cbrc78pcQHwfM&w=1707&h=1513&q=%EB%87%8C%20%2B%20%EA%B8%B0%EC%96%B4%20svg&ved=2ahUKEwjl96SpnL-FAxWnfvUHHZPDDesQMygBegQIARBU",
+    link: "https://lama.dev",
+  },
+  {
+    id: 2,
+    color: "from-blue-300 to-violet-300",
+    title: "Next.js Medium Blog",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fsvgsilh.com%2Fsvg%2F2099128.svg&tbnid=eeNPqSlmL4oxpM&vet=12ahUKEwjl96SpnL-FAxWnfvUHHZPDDesQMygBegQIARBU..i&imgrefurl=https%3A%2F%2Fsvgsilh.com%2Fko%2Fimage%2F2099128.html&docid=1Cbrc78pcQHwfM&w=1707&h=1513&q=%EB%87%8C%20%2B%20%EA%B8%B0%EC%96%B4%20svg&ved=2ahUKEwjl96SpnL-FAxWnfvUHHZPDDesQMygBegQIARBU",
+    link: "https://lama.dev",
+  },
+  {
+    id: 3,
+    color: "from-violet-300 to-purple-300",
+    title: "Vanilla Book App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fsvgsilh.com%2Fsvg%2F2099128.svg&tbnid=eeNPqSlmL4oxpM&vet=12ahUKEwjl96SpnL-FAxWnfvUHHZPDDesQMygBegQIARBU..i&imgrefurl=https%3A%2F%2Fsvgsilh.com%2Fko%2Fimage%2F2099128.html&docid=1Cbrc78pcQHwfM&w=1707&h=1513&q=%EB%87%8C%20%2B%20%EA%B8%B0%EC%96%B4%20svg&ved=2ahUKEwjl96SpnL-FAxWnfvUHHZPDDesQMygBegQIARBU",
+    link: "https://lama.dev",
+  },
+  {
+    id: 4,
+    color: "from-purple-300 to-red-300",
+    title: "Spotify Music App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fsvgsilh.com%2Fsvg%2F2099128.svg&tbnid=eeNPqSlmL4oxpM&vet=12ahUKEwjl96SpnL-FAxWnfvUHHZPDDesQMygBegQIARBU..i&imgrefurl=https%3A%2F%2Fsvgsilh.com%2Fko%2Fimage%2F2099128.html&docid=1Cbrc78pcQHwfM&w=1707&h=1513&q=%EB%87%8C%20%2B%20%EA%B8%B0%EC%96%B4%20svg&ved=2ahUKEwjl96SpnL-FAxWnfvUHHZPDDesQMygBegQIARBU",
+    link: "https://lama.dev",
+  },
+];
+
+export default items;
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -2,11 +2,15 @@
 
 import React, { useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
+import items from "./items";
+import Image from "next/image";
+import Link from "next/link";
 
 const PortfolioPage = () => {
   const ref = useRef();
   const { scrollYProgress } = useScroll({ target: ref });
-  const x = useTransform(scrollYProgress, [0, 1], ["0%,100%"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  console.log(items);
   return (
     <motion.div
       className="h-full"
@@ -14,9 +18,27 @@ const PortfolioPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-[600vh]">
+      <div className="h-[600vh] relative">
         <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
           My Works
+        </div>
+        <div className="sticky top-0 flex h-screen gap-4 items-center">
+          {items.map((res) => {
+            return (
+              <div className="" key={res.id}>
+                <div className="">
+                  <h1>{res.title}</h1>
+                  <div className="relative">
+                    {/* <Image src={res.img} alt="" fill /> */}
+                  </div>
+                  <p>{res.desc}</p>
+                  <Link href={res.link}>
+                    <button>See Demo</button>
+                  </Link>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </motion.div>
