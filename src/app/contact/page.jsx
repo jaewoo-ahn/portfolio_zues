81b8fff10e4562888c425ee041810486c8ddc1d3
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-04-15 19:47:51 +0900

    basic

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -10,7 +10,12 @@ const ContactPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      About
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/*CMT text container */}
+        <div className="h-1/2 lg:h-full lg:w-1/2"></div>
+        {/* CMT form container */}
+        <form className="h-1/2 lg:h-full lg:w-1/2"></form>
+      </div>
     </motion.div>
   );
 };
