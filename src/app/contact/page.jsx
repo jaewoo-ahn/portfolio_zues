c1a69e77c4bc295697e382f3aa639c67b7e4d39c
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-04-15 19:53:08 +0900

    feat : text animation

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,6 +3,8 @@
 import React from "react";
 import { motion } from "framer-motion";
 const ContactPage = () => {
+  const text = "say hello";
+
   return (
     <motion.div
       className="h-full"
@@ -12,7 +14,26 @@ const ContactPage = () => {
     >
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/*CMT text container */}
-        <div className="h-1/2 lg:h-full lg:w-1/2"></div>
+        <div className="h-1/2 lg:h-full lg:w-1/2">
+          <div>
+            {text.split("").map((res, idx) => {
+              return (
+                <motion.span
+                  key={idx}
+                  initial={{ opacity: 1 }}
+                  animate={{ opacity: 0 }}
+                  transition={{
+                    duration: 3,
+                    repeat: Infinity,
+                    delay: idx * 0.1,
+                  }}
+                >
+                  {res}
+                </motion.span>
+              );
+            })}
+          </div>
+        </div>
         {/* CMT form container */}
         <form className="h-1/2 lg:h-full lg:w-1/2"></form>
       </div>
